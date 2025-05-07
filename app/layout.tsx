00045331867f3fc4cbc419d7339d46a30f770bbc
puncharrow5 <osh4761@hanmail.net>
2025-05-07 22:00:50 +0900

    fix: 텍스트 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "랜딩페이지 에디터 웹 뷰어어",
+  title: "랜딩페이지 에디터 웹 뷰어",
   description: "랜딩페이지로 제작한 웹을 볼 수 있는 뷰어입니다.",
 };
 
